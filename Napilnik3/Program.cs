f9264fce5cbce8c52b8b5199d4e0e5276aaeb118
wstelo <118074707+wstelo@users.noreply.github.com>
2025-07-01 22:34:44 +0300

    commit 1

--- Napilnik3/Program.cs
+++ Napilnik3/Program.cs
@@ -1,61 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 
 namespace Napilnik3
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             Pathfinder fileLogPathfinder = new Pathfinder(new FileLogWritter());
             Pathfinder сonsoleLogPathfinder = new Pathfinder(new ConsoleLogWritter());        
             Pathfinder fridayLogPathfinder = new Pathfinder(new SecureConsoleLogWritter(new FileLogWritter()));
             Pathfinder fridayConsoleLogPathfinder = new Pathfinder(new SecureConsoleLogWritter(new ConsoleLogWritter()));
-            List<ILogger> _loggers = new List<ILogger>();
-            _loggers.Add(new ConsoleLogWritter());
-            _loggers.Add(new SecureConsoleLogWritter(new FileLogWritter()));
-            Pathfinder combinedLogPathfinder = new Pathfinder(new CombinedLogWritter(_loggers));
-            
+            List<ILogger> loggers = new List<ILogger>();
+            loggers.Add(new ConsoleLogWritter());
+            loggers.Add(new SecureConsoleLogWritter(new FileLogWritter()));
+            Pathfinder combinedLogPathfinder = new Pathfinder(new CombinedLogWritter(loggers));            
             fileLogPathfinder.Find();
             сonsoleLogPathfinder.Find();
             fridayLogPathfinder.Find();
             fridayConsoleLogPathfinder.Find();
             combinedLogPathfinder.Find();
-
             Console.ReadKey();
         }
     }
 
     class Pathfinder
     {
-        private ILogger _logger;
-        private string text = "Что-то";
+        private readonly ILogger _logger;
+        private string _text = "Что-то";
 
         public Pathfinder(ILogger logger)
         {
+            if(logger == null)
+            {
+                throw new ArgumentNullException(logger.ToString());
+            }
+
             _logger = logger;
         }
 
         public void Find()
         {
-            _logger.WriteError(text);
+            _logger.WriteError(_text);
         }
     }
 
     class CombinedLogWritter : ILogger
     {
-        private List<ILogger> _loggers;
+        private readonly List<ILogger> _loggers;
 
         public CombinedLogWritter(List<ILogger> loggers)
         {
+            if (loggers.Count == 0)
+            {
+                throw new ArgumentNullException(loggers.ToString());
+            }
+
             _loggers = loggers;
         }
 
         public void WriteError(string message)
         {
+            if(message == null)
+            {
+                throw new ArgumentNullException(message.ToString());
+            }
+
             foreach (var logger in _loggers)
             {
                 logger.WriteError(message);
@@ -65,9 +76,16 @@ namespace Napilnik3
 
     class FileLogWritter : ILogger
     {
+        private const string FileName = "log.txt";
+
         public void WriteError(string message)
         {
-            File.WriteAllText("log.txt", message);
+            if (message == null)
+            {
+                throw new ArgumentNullException(message.ToString());
+            }
+
+            File.WriteAllText(FileName, message);
         }
     }
 
@@ -75,24 +93,38 @@ namespace Napilnik3
     {
         public void WriteError(string message)
         {
+            if (message == null)
+            {
+                throw new ArgumentNullException(message.ToString());
+            }
+
             Console.WriteLine(message);
         }
     }
 
     class SecureConsoleLogWritter : ILogger
     {
-        private ILogger _logger;
-        private DayOfWeek _friday = DayOfWeek.Friday;
-        private DateTime _dateTime = DateTime.Now;
+        private readonly ILogger _logger;
+        private DayOfWeek _requiredDayOfTheWeek = DayOfWeek.Friday;
 
         public SecureConsoleLogWritter(ILogger logger)
         {
+            if (logger == null)
+            {
+                throw new ArgumentNullException(logger.ToString());
+            }
+
             _logger = logger;
         }
 
         public void WriteError(string message)
         {
-            if (_dateTime.DayOfWeek == _friday)
+            if (message == null)
+            {
+                throw new ArgumentNullException(message.ToString());
+            }
+
+            if (DateTime.Now.DayOfWeek == _requiredDayOfTheWeek)
             {
                 _logger.WriteError(message);
             }
